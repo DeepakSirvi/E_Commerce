d097ce7f4641952f30ec0a6661ebb0f44d91a7d0
DeepakSirvi <deepaksirvi45@gmail.com>
2024-03-02 11:34:49 +0530

    Deepak

--- src/main/java/com/ecommerce/EcommerceApplication.java
+++ src/main/java/com/ecommerce/EcommerceApplication.java
@@ -2,12 +2,20 @@ package com.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 public class EcommerceApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
+		Runtime runtime = Runtime.getRuntime();
+		System.out.println("Max memory " + runtime.maxMemory()/(1024*1024));
+		System.out.println("Total memory " + runtime.totalMemory()/(1024*1024));
+		System.out.println("Free memory " + runtime.freeMemory()/(1024*1024));
+		System.out.println("Available Proccessor " + runtime.availableProcessors());
+		System.out.println("Used memory " + (runtime.totalMemory() - runtime.freeMemory())/(1024*1024));
 		}
+	
+	
+	
 }
