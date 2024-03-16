50eb07e6853002b70b1ea128e953f085eab22e46
DeepakSirvi <deepaksirvi45@gmail.com>
2024-03-16 10:51:53 +0530

    Image

--- src/main/java/com/ecommerce/controller/BrandController.java
+++ src/main/java/com/ecommerce/controller/BrandController.java
@@ -13,14 +13,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.ecommerce.payload.AddressRequest;
-import com.ecommerce.payload.AddressResponse;
 import com.ecommerce.payload.BrandRequest;
 import com.ecommerce.payload.BrandResponse;
 import com.ecommerce.service.BrandService;
